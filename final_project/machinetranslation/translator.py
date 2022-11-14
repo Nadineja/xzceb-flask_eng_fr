c127cecb839e1b688219921a46880ccbcad89286
Nadineja <jammetnadine@gmail.com>
2022-11-14 15:58:28 -0500

    Final changes

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -29,12 +29,7 @@ def english_to_french(english_text):
         return french_text
 
     response = language_translator.translate(text=english_text, model_id="en-fr").get_result()
-    french_text = json.dumps(response, indent=2, ensure_ascii=False)
-    french_text1 = json.loads(french_text)
-    first_value = list(french_text1.values())[0]
-    f_translation_dict = first_value[0]
-    f_translation = list(f_translation_dict.values())[0]
-    print(f_translation)
+    f_translation = response['translations'][0]['translation']
     return f_translation
 
 
@@ -47,14 +42,8 @@ def french_to_english(french_text):
     """
     if french_text is None:
         english_text = "No string has been entered"
-        print(english_text)
         return english_text
 
     response = language_translator.translate(text=french_text, model_id="fr-en").get_result()
-    english_text = json.dumps(response, indent=2, ensure_ascii=False)
-    english_text1 = json.loads(english_text)
-    first_value = list(english_text1.values())[0]
-    e_translation_dict = first_value[0]
-    e_translation = list(e_translation_dict.values())[0]
-    print(e_translation)
+    e_translation = response['translations'][0]['translation']  
     return e_translation
